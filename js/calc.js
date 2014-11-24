3eeb546684df5c8f53d17fce333c1c6ae229b318
PaavoNelimarkka <paavo.nelimarkka@gmail.com>
2014-11-24 15:49:34 +0200

    add js delete stupid npm log

---
+++ js/calc.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	// get variables from form elements
+	var alc = $(document).get("alc");
+	var cap = $(document).get("cap");
+	var price = $(document).get("price");
+
+});
