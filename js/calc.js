b389ef8b7d229413a19425953b335f79a10881f2
PaavoNelimarkka <paavo.nelimarkka@gmail.com>
2014-11-25 11:59:01 +0200

    edit calc.js to have floats instead of ints

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
 	$("#count").on("click", function () {
-		var alc = parseInt($("#alc").val());
-		var cap = parseInt($("#cap").val());
-		var price = parseInt($("#price").val());
+		var alc = parseFloat($("#alc").val());
+		var cap = parseFloat($("#cap").val());
+		var price = parseFloat($("#price").val());
 		
 		$("#result").val((cap*(alc/100)/price).toFixed(2));
 	});
